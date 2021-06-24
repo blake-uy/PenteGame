40af2880f63f98271790e2827c751127e7b51167
blake-uy <58790294+blake-uy@users.noreply.github.com>
2021-06-24 16:03:11 -0700

    Changed package name

---
+++ src/main/java/Domain/Example.java
@@ -0,0 +1,14 @@
+package Domain;
+
+public class Example {
+    public String greeting;
+
+    public Example(String s) {
+        greeting = s;
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+}
